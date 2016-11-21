ecda01668ae0c5d70134f53b5fb0fb99ece5edca
Jeb Bearer <jbearer@hmc.edu>
2016-11-20 17:45:02 -0800

    Create subcommands for compiling and playing back.
    cspp compile <source> [-o <file>]
      Compile the CSound++ program in <source>, and optionally store the
      output in <file>. (Prints to stdout by default.)
    
    cspp play <orc> <score> [-o <file>]
      Play the compiled orchestra file <orc> using the MIDI score contained
      in <score>. Optionally write output as a WAV to <file>, otherwise play
      over the DAC.

---
+++ src/main/scala/csoundpp/files/FileReader.scala
@@ -0,0 +1,34 @@
+package cspp
+
+import java.io.File
+import java.nio.file.{Paths, Files}
+
+case class CsppFile(path: String, contents: String)
+
+object CsppFileReader {
+
+  def apply(p: String): Either[CsppCompileError, CsppFile] =
+    for {
+      path <- absPath(p).right
+      contents <- readFile(path).right
+    } yield CsppFile(path, contents + "\n") // Add a newline at the end so we can concatenate files easily
+
+  def absPath(path: String): Either[CsppCompileError, String] =
+    if (Files.exists(Paths.get(path))) {
+      // First we try treating the expression as an absolute path or a path relative to the current
+      // directory
+      Right(path)
+    } else if (Files.exists(Paths.get("resources" + File.separator + path))) {
+      // If that failed, we look in resources
+      Right("resources" + File.separator + path)
+    } else {
+      Left(new CsppFileError(s"Cannot open file '$path'."))
+    }
+
+  def readFile(path: String): Either[CsppCompileError, String] =
+    try {
+      Right(io.Source.fromFile(path).getLines.mkString("\n"))
+    } catch {
+      case err: java.io.IOException => Left(new CsppFileError(err.getMessage))
+    }
+}
