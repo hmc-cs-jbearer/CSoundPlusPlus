248e67366c9b618cec6aa1a76d6e4f0595e3c895
Jeb Bearer <jbearer@hmc.edu>
2016-12-06 14:02:49 -0800

    Override CsppToken.toString to enable better error messages at parse time

--- src/main/scala/csoundpp/parser/Tokens.scala
+++ src/main/scala/csoundpp/parser/Tokens.scala
@@ -4,23 +4,25 @@ import scala.util.parsing.input.Positional
 
 import cspp._
 
-trait CsppToken extends CsppPositional
+abstract class CsppToken(name: String) extends CsppPositional {
+    override def toString = name
+}
 
-case class IDENT(str: String) extends CsppToken
-case class NUMBER(num: Double) extends CsppToken
-case class FILE(str: String) extends CsppToken
+case class IDENT(str: String) extends CsppToken(s"identifier ($str)")
+case class NUMBER(num: Double) extends CsppToken(s"number literal (${num.toString})")
+case class FILE(str: String) extends CsppToken(s"file ($str)")
 
 // Sadly, these must all be case classes (not objects) because they have a mutable position field
-case class IMPORT() extends CsppToken
-case class INSTR() extends CsppToken
-case class PARALLEL() extends CsppToken
-case class LPAREN() extends CsppToken
-case class RPAREN() extends CsppToken
-case class LBRACE() extends CsppToken
-case class RBRACE() extends CsppToken
-case class COMMA() extends CsppToken
-case class EQUALS() extends CsppToken
-case class STAR() extends CsppToken
-case class SLASH() extends CsppToken
-case class PLUS() extends CsppToken
-case class MINUS() extends CsppToken
+case class IMPORT() extends CsppToken("keyword (import)")
+case class INSTR() extends CsppToken("keyword (instr)")
+case class PARALLEL() extends CsppToken("keyword (parallel)")
+case class LPAREN() extends CsppToken("(")
+case class RPAREN() extends CsppToken(")")
+case class LBRACE() extends CsppToken("{")
+case class RBRACE() extends CsppToken("}")
+case class COMMA() extends CsppToken(",")
+case class EQUALS() extends CsppToken("=")
+case class STAR() extends CsppToken("*")
+case class SLASH() extends CsppToken("/")
+case class PLUS() extends CsppToken("+")
+case class MINUS() extends CsppToken("-")
