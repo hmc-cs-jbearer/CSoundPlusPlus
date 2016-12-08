827618db123f1bb53f39c517e231498e7cd7b902
Jeb Bearer <jbearer@hmc.edu>
2016-12-08 01:07:10 -0800

    When given no arguments, display usage instead of trying to compile an empty filename

--- src/main/scala/csoundpp/Cspp.scala
+++ src/main/scala/csoundpp/Cspp.scala
@@ -5,8 +5,9 @@ import scopt.OptionParser
 object Cspp extends App {
   val config = ArgParser(args)
   config.command match {
-    case Compile => CsppCompiler(config)
-    case Play => CsppPlayer(config)
+    case Some(Compile) => CsppCompiler(config)
+    case Some(Play) => CsppPlayer(config)
+    case _ => ArgParser.showUsage()
   }
 }
 
@@ -15,7 +16,7 @@ case object Compile extends Command
 case object Play extends Command
 
 case class Config(
-  command: Command = Compile,
+  command: Option[Command] = None,
 
   inFile: String = "",
   outFile: String = "",
@@ -26,76 +27,78 @@ case class Config(
 
 object ArgParser {
 
-  def apply(argv: Array[String]): Config = {
+  def apply(argv: Array[String]): Config =
+    parser.parse(argv, Config()) match {
+      case Some(config) => config
+      case None         => { System.exit(1); Config() }
+    }
 
-    new OptionParser[Config]("cspp") {
+  def showUsage() = parser.showUsage
 
-      head(BuildInfo.name, BuildInfo.version)
+  val parser = new OptionParser[Config]("cspp") {
 
-      cmd("compile").
-        text("compile a CSound++ program").
+    head(BuildInfo.name, BuildInfo.version)
+
+    cmd("compile").
+      text("compile a CSound++ program").
+      maxOccurs(1).
+      action( (_, c) => c.copy(command = Some(Compile)) ).
+      children(
+
+        opt[Unit]('h', "human-readable").
+          text("generate readble CSound code (results in larger output files)").
+          action { (_, c) =>
+            c.copy(humanReadable = true)
+          },
+
+        opt[Unit]('d', "debug").
+          text("compile with debug information, which can be used by CSPP analysis tools").
+          action { (_, c) =>
+            c.copy(debug = true)
+          },
+
+        opt[String]('o', "out").
+          text("place the output into <file> (default is stdout)").
+          valueName("<file>").
+          action { (x, c) =>
+            c.copy(outFile = x)
+          },
+
+        arg[String]("file").required().
+          text("the CSound++ source file to compile").
+          action { (x, c) =>
+            c.copy(inFile = x)
+          }
+      )
+
+      cmd("play").
+        text("play a MIDI score with a compiled CSound++ program").
         maxOccurs(1).
-        action( (_, c) => c.copy(command = Compile) ).
+        action( (_, c) => c.copy(command = Some(Play)) ).
         children(
 
-          opt[Unit]('h', "human-readable").
-            text("generate readble CSound code (results in larger output files)").
-            action { (_, c) =>
-              c.copy(humanReadable = true)
-            },
-
-          opt[Unit]('d', "debug").
-            text("compile with debug information, which can be used by CSPP analysis tools").
-            action { (_, c) =>
-              c.copy(debug = true)
-            },
-
           opt[String]('o', "out").
-            text("place the output into <file> (default is stdout)").
             valueName("<file>").
+            text("place the output into <file> in WAV format").
             action { (x, c) =>
               c.copy(outFile = x)
             },
 
-          arg[String]("file").required().
-            text("the CSound++ source file to compile").
+          arg[String]("program").required().
+            text("the file containing the program to run").
             action { (x, c) =>
               c.copy(inFile = x)
+            },
+
+          arg[String]("score").required().
+            text("the MIDI file containing the score to perform").
+            action { (x, c) =>
+              c.copy(score = x)
             }
+
         )
 
-        cmd("play").
-          text("play a MIDI score with a compiled CSound++ program").
-          maxOccurs(1).
-          action( (_, c) => c.copy(command = Play) ).
-          children(
+    help("help").text("display this help and exit")
 
-            opt[String]('o', "out").
-              valueName("<file>").
-              text("place the output into <file> in WAV format").
-              action { (x, c) =>
-                c.copy(outFile = x)
-              },
-
-            arg[String]("program").required().
-              text("the file containing the program to run").
-              action { (x, c) =>
-                c.copy(inFile = x)
-              },
-
-            arg[String]("score").required().
-              text("the MIDI file containing the score to perform").
-              action { (x, c) =>
-                c.copy(score = x)
-              }
-
-          )
-
-      help("help").text("display this help and exit")
-
-    }.parse(argv, Config()) match {
-      case Some(config) => config
-      case None         => { System.exit(1); Config() }
-    }
   }
 }
