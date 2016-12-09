85afedcb483cb5194c72ba60b8917637d8851adf
Jeb Bearer <jbearer@hmc.edu>
2016-12-08 23:24:54 -0800

    Retry flaky curl command 5 times before we give up

--- bin/osx_install.sh
+++ bin/osx_install.sh
@@ -8,7 +8,20 @@ brew install python3
 
 # Install CSound
 CSOUND_PKG="csound6.06-OSX-universal"
-curl -sL -m 900 "https://sourceforge.net/projects/csound/files/csound6/Csound6.06/$CSOUND_PKG.dmg/" > "$CSOUND_PKG.dmg"
+
+CURL_MAX_RETRIES=5
+try=1
+until [[ $try == $CURL_MAX_RETRIES ]]; do
+    curl -L -m 900 "https://sourceforge.net/projects/csound/files/csound6/Csound6.06/$CSOUND_PKG.dmg/" > "$CSOUND_PKG.dmg" && break
+    echo "curl failed ($?). Retrying in 1 minute."
+    let try=$try+1
+    sleep 60
+done
+if [[ $try == $CURL_MAX_RETRIES ]]; then
+    echo "Failed to download CSound binary. Failed $CURL_MAX_RETRIES attempts."
+    exit 1
+fi
+
 sudo hdiutil attach "$CSOUND_PKG.dmg"
 sudo installer -package "/Volumes/Csound 6.06/$CSOUND_PKG.pkg" -target /
 sudo hdiutil detach "/Volumes/Csound 6.06"
@@ -19,9 +32,3 @@ mkdir -p /Users/travis/bin
 sudo curl -s https://raw.githubusercontent.com/paulp/sbt-extras/master/sbt > /Users/travis/bin/sbt
 sudo chmod 0755 /Users/travis/bin/sbt
 which sbt || true
-
-# We're running with the sbt launcher in the project root directory, but the integration tests call
-# sbt expecting it to be on the path. So we fix that with a bit of a hack here.
-
-#export PATH="`pwd`:$PATH"
-echo $PATH
